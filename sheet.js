b46c3a438aa39427852caf73fd27684811907b44
ewf-updater <ewf-updater@ewf.org>
2018-07-17 10:48:29 +0200

    sheets

--- sheet.js
+++ sheet.js
@@ -1,25 +1,11 @@
 $(document).ready(function(e) {
-
     $('.stat').bind('input', function() {
-        var inputName = $(this).attr('name')
-        var mod = parseInt($(this).val()) - 10
-
-        if (mod % 2 == 0)
-            mod = mod / 2
-        else
-            mod = (mod - 1) / 2
-
-        if (isNaN(mod))
-            mod = ""
-        else if (mod >= 0)
-            mod = "+" + mod
-
-        var scoreName = inputName.slice(0, inputName.indexOf("score"))
-        var modName = scoreName + "mod"
-
-        $("[name='" + modName + "']").val(mod)
+        updateMod($(this));
     })
-
+    console.log($("[name='proficiencybonus']"));
+    $("[name='proficiencybonus']").bind('input', function() {
+        updateProfBonus();
+    })
     $('.statmod').bind('change', function() {
         var name = $(this).attr('name')
         name = "uses" + name.slice(0, name.indexOf('mod'))
@@ -57,6 +43,114 @@ $(document).ready(function(e) {
 
 });
 
+function autofill() {
+    var url = window.location.pathname;
+    var name = url.substring(url.lastIndexOf('/') + 1).split(".")[0];
+    fill(name);
+}
+
+function fill(name) {
+    var sheet = settings[name]["sheet"];
+    var newTitle = settings[name]["title"];
+    if (document.title != newTitle) {
+        document.title = newTitle;
+    }
+    for (key in sheet["description"]) {
+        var char = $("[name='" + key + "']");
+        char.val(sheet["description"][key]);
+    }
+
+    for (key in sheet["chars"]) {
+        var char = $("[name='" + key + "']");
+        char.val(sheet["chars"][key]);
+        updateMod(char);
+    }
 
 
-var andrea = {};
+    $("[name='proficiencybonus']").val(sheet["proficiencybonus"]);
+    for (var i = 0; i < sheet["skills"].length; i++) {
+        var name = sheet["skills"][i];
+        var skill = $("[name='" + name + "']");
+        var label = $('label[for=' + name + ']').text();
+        var char = label.split("(")[1].split(")")[0];
+        $("[name='" + name + "-prof']").prop("checked", true);
+        var charMod = 0;
+        $(".statmod").each(function() {
+
+            if ($(this).attr('name').startsWith(char)) {
+                charMod = $(this).val();
+                skill.val(parseInt($("[name='proficiencybonus']").val()) + parseInt(charMod));
+            }
+        })
+    }
+    for (var i = 0; i < sheet["saving"].length; i++) {
+        var name = sheet["saving"][i] + "-save";
+        var saving = $("[name='" + name + "']");
+        var label = $('label[for=' + name + ']').text();
+        var char = sheet["saving"][i];
+        $("[name='" + name + "-prof']").prop("checked", true);
+        var charMod = 0;
+        $(".statmod").each(function() {
+            if ($(this).attr('name').startsWith(char)) {
+                charMod = $(this).val();
+                saving.val(parseInt($("[name='proficiencybonus']").val()) + parseInt(charMod));
+            }
+        })
+    }
+
+
+}
+
+function updateMod(char) {
+    var inputName = char.attr('name')
+    var mod = parseInt(char.val()) - 10
+
+    if (mod % 2 == 0)
+        mod = mod / 2
+    else
+        mod = (mod - 1) / 2
+
+    if (isNaN(mod))
+        mod = ""
+    else if (mod >= 0)
+        mod = "+" + mod
+
+    var scoreName = inputName.slice(0, inputName.indexOf("score"))
+    var modName = scoreName + "mod"
+
+    $("[name='" + modName + "']").val(mod)
+}
+
+
+
+
+
+var settings = {
+    "andrea": {
+        "title": "Andrea",
+        "sheet": {
+            "description": {
+                "charname": "Andrea",
+                "classlevel": "Fighter 6",
+                "race": "Human",
+                "alignment": "neutral good",
+                "background": "outlander",
+                "experiencepoints": 15000
+            },
+            "chars": {
+                "Strengthscore": 16,
+                "Dexterityscore": 11,
+                "Constitutionscore": 12,
+                "Intelligencescore": 16,
+                "Wisdomscore": 10,
+                "Charismascore": 10
+            },
+            "skills": ["Athletics", "Insight"],
+            "saving": ["Strength", "Constitution"],
+            "proficiencybonus": 3,
+            "equipment": {}
+        }
+    },
+    "mirko": { "title": "Mirko", "sheet": { "classlevel": "Warrior 3" } },
+    "lisa": { "title": "Lisa", "sheet": { "classlevel": "Warrior 3" } }
+};
